d949a3a6212ab8760420163f098c2ddf9cfbfeae
nidhi-singh02 <trippin@berachain.com>
2024-09-30 14:33:26 +0530

    add bartio network
    Signed-off-by: nidhi-singh02 <trippin@berachain.com>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config()
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
@@ -8,4 +9,10 @@ require("./tasks/faucet");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    bartio: {
+      url: "https://bartio.rpc.berachain.com",
+      accounts: [process.env.WALLET_KEY]
+    },
+  },
 };
